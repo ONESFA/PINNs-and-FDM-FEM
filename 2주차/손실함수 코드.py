0cf2716cf3ae868992a781ed7c8a93cf4eb8478f
ONESFA <coax6900@gmail.com>
2026-09-21 22:08:42 +0900

    Update 손실함수 코드.py

--- 2주차/손실함수 코드.py
+++ 2주차/손실함수 코드.py
@@ -1,7 +1,19 @@
-#기본적 PDE LOSS
-def loss_pde(model, x_f, y_f, t_f, c=1.0):
+def total_loss(
+    model,
 
-    residual = pde_residual(
+    x_f, y_f, t_f,
+
+    x_bc, y_bc, t_bc,
+
+    x_ic, y_ic,
+    u_ic,
+    v_ic,
+
+    c=1.0
+):
+
+    # PDE Loss
+    L_pde = loss_pde(
         model,
         x_f,
         y_f,
@@ -9,67 +21,31 @@ def loss_pde(model, x_f, y_f, t_f, c=1.0):
         c
     )
 
-    return torch.mean(residual**2)
-
-#초기 조건 LOSS
-def loss_ic(model, x_ic, y_ic, u_ic):
-
-    t_ic = torch.zeros_like(x_ic)
-
-    u_pred = model(
-        x_ic,
-        y_ic,
-        t_ic
-    )
-
-    return torch.mean((u_pred - u_ic)**2)
-
-#경계 조건 LOSS
-def loss_bc(model, x_bc, y_bc, t_bc):
-
-    u_pred = model(
+    # Boundary Condition Loss
+    L_bc = loss_bc(
+        model,
         x_bc,
         y_bc,
         t_bc
     )
 
-    # u = 0
-    return torch.mean(u_pred**2)
-
-#최종 조건 LOSS
-def total_loss(
-    model,
-    x_f, y_f, t_f,
-    x_bc, y_bc, t_bc,
-    x_ic, y_ic, u_ic,
-    v_ic
-):
-
-    # PDE
-    L_pde = loss_pde(
-        model,
-        x_f, y_f, t_f
-    )
-
-    # Boundary condition
-    L_bc = loss_bc(
-        model,
-        x_bc, y_bc, t_bc
-    )
-
-    # Initial displacement
+    # Initial Displacement Loss
     L_ic = loss_ic(
         model,
-        x_ic, y_ic, u_ic
+        x_ic,
+        y_ic,
+        u_ic
     )
 
-    # Initial velocity
+    # Initial Velocity Loss
     L_ic_velocity = loss_ic_velocity(
         model,
-        x_ic, y_ic, v_ic
+        x_ic,
+        y_ic,
+        v_ic
     )
 
-    # 기본적인 PINN loss
+    # Total
     loss = (
         L_pde
         + L_bc
@@ -77,4 +53,10 @@ def total_loss(
         + L_ic_velocity
     )
 
-    return loss, L_pde, L_bc, L_ic, L_ic_velocity
+    return (
+        loss,
+        L_pde,
+        L_bc,
+        L_ic,
+        L_ic_velocity
+    )
